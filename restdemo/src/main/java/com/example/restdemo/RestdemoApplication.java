8aee54edc64fac45205c5d801612d4669a639cc4
ERomanenko111 <romanovc2001@yandex.ru>
2024-07-01 12:37:31 +0300

    first1

--- restdemo/src/main/java/com/example/restdemo/RestdemoApplication.java
+++ restdemo/src/main/java/com/example/restdemo/RestdemoApplication.java
@@ -2,10 +2,7 @@ package com.example.restdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-
-@ComponentScan("com.example.restdemo")
 @SpringBootApplication
 public class RestdemoApplication {
 
--- restdemo/src/main/java/com/example/restdemo/controller/PersonController.java
+++ restdemo/src/main/java/com/example/restdemo/controller/PersonController.java
@@ -8,30 +8,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
 
 @RestController
+@RequestMapping("/api")
 public class PersonController {
 
-    private final List<Person> persons = new ArrayList<>(Arrays.asList(
-            new Person(1, "Ivan", "Ivanovich", "Ivanov", LocalDate.of(1999, 2,3)),
-            new Person(2, "Петр", "Петрович", "Петров", LocalDate.of(2002, 2,2)),
-            new Person(3, "Евгений", "Васильевич", "Васин", LocalDate.of(2005, 4,8)),
-            new Person(4, "Максим", "Яковлевич", "Окопский", LocalDate.of(1978, 6,5))
-    ));
-
+    private final PersonRepository repository;
 
     @Autowired
-    private PersonRepository repository;
-
+    public PersonController(PersonRepository repository) {
+        this.repository = repository;
+    }
 
     @PostMapping("/person")
     public Person addPerson(@RequestBody Person person) {
-        repository.save(person);
-        return person;
+        return repository.save(person);
     }
 
     @PutMapping("/person/{id}")
@@ -46,8 +37,10 @@ public class PersonController {
     }
 
     @GetMapping("/person/{id}")
-    public Optional<Person> findPersonById(@PathVariable int id) {
-        return repository.findById(id);
+    public ResponseEntity<Person> findPersonById(@PathVariable int id) {
+        return repository.findById(id)
+                .map(person -> ResponseEntity.ok().body(person))
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/person/{id}")
